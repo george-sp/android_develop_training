547a7fbdef2433b969c05d4432013da3697de0b2
george_spiridakis <spiridakis.grg@gmail.com>
2016-05-14 04:19:21 +0300

    Use MainActivity as a navigator.

---
+++ 01. Getting Started/02. Supporting Different Devices/SupportDifferentDevices/app/src/main/java/com/example/george/supportdifferentdevices/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.george.supportdifferentdevices;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startDifferentLanguages(View view) {
+        Intent intent = new Intent(this, DifferentLanguagesActivity.class);
+        startActivity(intent);
+    }
+
+    public void startDifferentScreens(View view) {
+        Intent intent = new Intent(this, DifferentScreensActivity.class);
+        startActivity(intent);
+    }
+
+}
