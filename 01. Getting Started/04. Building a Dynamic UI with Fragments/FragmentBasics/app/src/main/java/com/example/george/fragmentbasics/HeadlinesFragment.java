a0ed98b2ab98e21eeb7868e0637fdca2ea2a44cb
george_spiridakis <spiridakis.grg@gmail.com>
2016-05-16 17:25:51 +0300

    Communicate with Other Fragments.

--- 01. Getting Started/04. Building a Dynamic UI with Fragments/FragmentBasics/app/src/main/java/com/example/george/fragmentbasics/HeadlinesFragment.java
+++ 01. Getting Started/04. Building a Dynamic UI with Fragments/FragmentBasics/app/src/main/java/com/example/george/fragmentbasics/HeadlinesFragment.java
@@ -1,11 +1,12 @@
 package com.example.george.fragmentbasics;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 /**
  * @author George Spiridakis <george@codeburrow.com>
@@ -14,11 +15,39 @@ import android.view.ViewGroup;
  * ---------->    http://codeburrow.com    <----------
  * ===================================================
  */
-public class HeadlinesFragment extends Fragment {
+public class HeadlinesFragment extends ListFragment {
 
-    @Nullable
+    OnHeadlineSelectedListener mCallback;
+
+    // Container Activity must implement this interface
+    public interface OnHeadlineSelectedListener {
+
+        public void onArticleSelected(int position);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        // This makes sure that the container activity has implemented
+        // the callback interface. If not, it throws an exception
+        try {
+            mCallback = (OnHeadlineSelectedListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnHeadlineSelectedListener");
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.article_view, container);
     }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        // Send the event to the host activity
+        mCallback.onArticleSelected(position);
+    }
+
 }
--- 01. Getting Started/04. Building a Dynamic UI with Fragments/FragmentBasics/app/src/main/java/com/example/george/fragmentbasics/MainActivity.java
+++ 01. Getting Started/04. Building a Dynamic UI with Fragments/FragmentBasics/app/src/main/java/com/example/george/fragmentbasics/MainActivity.java
@@ -12,7 +12,41 @@ import android.support.v7.app.AppCompatActivity;
  * If you're using the v7 appcompat library, your activity should instead extend
  * AppCompatActivity, which is a subclass of FragmentActivity.
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements HeadlinesFragment.OnHeadlineSelectedListener{
+
+    @Override
+    public void onArticleSelected(int position) {
+        // The user selected the headline of an article from the HeadlinesFragment
+        // Do something here to display that article
+
+        ArticleFragment articleFrag = (ArticleFragment)
+                getSupportFragmentManager().findFragmentById(R.id.article_fragment);
+
+        if (articleFrag != null) {
+            // If article frag is available, we're in two-pane layout...
+
+            // Call a method in the ArticleFragment to update its content
+            articleFrag.updateArticleView(position);
+        } else {
+            // Otherwise, we're in the one-pane layout and must swap frags...
+
+            // Create fragment and give it an argument for the selected article
+            ArticleFragment newFragment = new ArticleFragment();
+            Bundle args = new Bundle();
+            args.putInt(ArticleFragment.ARG_POSITION, position);
+            newFragment.setArguments(args);
+
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+            // Replace whatever is in the fragment_container view with this fragment,
+            // and add the transaction to the back stack so the user can navigate back
+            transaction.replace(R.id.fragment_container, newFragment);
+            transaction.addToBackStack(null);
+
+            // Commit the transaction
+            transaction.commit();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
      * the fragment, it restarts. If you do not add the transaction to the back stack, then the
      * fragment is destroyed when removed or replaced.
      */
-    public void replaceOnFragmentWithAnother() {
+    /*public void replaceOnFragmentWithAnother() {
         // Create fragment and give it an argument specifying the article it should show
         ArticleFragment newFragment = new ArticleFragment();
         Bundle args = new Bundle();
@@ -65,6 +99,6 @@ public class MainActivity extends AppCompatActivity {
 
         // Commit the transaction
         transaction.commit();
-    }
+    }*/
 
 }
