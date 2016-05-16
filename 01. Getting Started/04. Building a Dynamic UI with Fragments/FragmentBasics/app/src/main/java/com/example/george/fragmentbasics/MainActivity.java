a6c4d7102402149426fe8ec63c82f19d9712fbb6
george_spiridakis <spiridakis.grg@gmail.com>
2016-05-16 17:11:08 +0300

    Build a flexible UI.

--- 01. Getting Started/04. Building a Dynamic UI with Fragments/FragmentBasics/app/src/main/java/com/example/george/fragmentbasics/MainActivity.java
+++ 01. Getting Started/04. Building a Dynamic UI with Fragments/FragmentBasics/app/src/main/java/com/example/george/fragmentbasics/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.george.fragmentbasics;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 /*
@@ -17,5 +18,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news_articles);
+
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            ArticleFragment firstFragment = new ArticleFragment();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            firstFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, firstFragment).commit();
+        }
     }
+
+    /*
+     * Note: When you remove or replace a fragment and add the transaction to the back stack, the
+     * fragment that is removed is stopped (not destroyed). If the user navigates back to restore
+     * the fragment, it restarts. If you do not add the transaction to the back stack, then the
+     * fragment is destroyed when removed or replaced.
+     */
+    public void replaceOnFragmentWithAnother() {
+        // Create fragment and give it an argument specifying the article it should show
+        ArticleFragment newFragment = new ArticleFragment();
+        Bundle args = new Bundle();
+        args.putInt(ArticleFragment.ARG_POSITION, position);
+        newFragment.setArguments(args);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack so the user can navigate back
+        transaction.replace(R.id.fragment_container, newFragment);
+        transaction.addToBackStack(null);
+
+        // Commit the transaction
+        transaction.commit();
+    }
+
 }
