b63d6da34a27091bc97ed5697a16c7e6c59e1885
George Spiridakis <spiridakis.grg@gmail.com>
2016-06-24 01:46:55 +0300

    Test for Android Beam File Transfer Support.

--- 02. Building Apps with Content Sharing/03. Sharing Files with NFC/SendFilesWithNFC/app/src/main/java/com/codeburrow/sendfileswithnfc/MainActivity.java
+++ 02. Building Apps with Content Sharing/03. Sharing Files with NFC/SendFilesWithNFC/app/src/main/java/com/codeburrow/sendfileswithnfc/MainActivity.java
@@ -1,13 +1,48 @@
 package com.codeburrow.sendfileswithnfc;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.nfc.NfcAdapter;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    NfcAdapter mNfcAdapter;
+    // Flag to indicate that Android Beam is available.
+    boolean mAndroidBeamAvailable = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // NFC isn't available on the device
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_NFC)) {
+            /*
+             * Disable NFC features here.
+             * For example, disable menu items or buttons that activate
+             * NFC-related features
+             */
+            Log.e(LOG_TAG, "NFC isn't available on device.");
+        }
+        // Android Beam file transfer isn't supported
+        else if (Build.VERSION.SDK_INT <
+                Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            // If Android Beam isn't available, don't continue.
+            mAndroidBeamAvailable = false;
+            /*
+             * Disable Android Beam file transfer features here.
+             */
+            Log.e(LOG_TAG, "Android Beam file transfer isn't supported.");
+        }
+        // Android Beam file transfer is available, continue.
+        else {
+            mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+            Log.e(LOG_TAG, "Android Beam file transfer is available, continue.");
+        }
     }
 }
