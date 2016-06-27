eb84c38f1ddd919ec9af3f8e1fb95c823352db2b
George Spiridakis <spiridakis.grg@gmail.com>
2016-06-28 00:41:54 +0300

    Get the directory from a content URI.

--- 02. Building Apps with Content Sharing/03. Sharing Files with NFC/ReceiveFilesWithNFC/app/src/main/java/com/codeburrow/receivefileswithnfc/MainActivity.java
+++ 02. Building Apps with Content Sharing/03. Sharing Files with NFC/ReceiveFilesWithNFC/app/src/main/java/com/codeburrow/receivefileswithnfc/MainActivity.java
@@ -1,8 +1,10 @@
 package com.codeburrow.receivefileswithnfc;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 
@@ -47,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
             if (TextUtils.equals(beamUri.getScheme(), "file")) {
                 mParentPath = new File(handleFileUri(beamUri));
             } else if (TextUtils.equals(beamUri.getScheme(), "content")) {
-                mParentPath = handleContentUri(beamUri);
+                mParentPath = new File(handleContentUri(beamUri));
             }
         }
     }
@@ -61,4 +63,62 @@ public class MainActivity extends AppCompatActivity {
         return copiedFile.getParent();
     }
 
+    /*
+     * Android Beam file transfer indexes the media files it transfers by running Media Scanner on the directory where it stores transferred files.
+     *
+     * Media Scanner writes its results to the MediaStore content provider,
+     * then it passes a content URI for the first file back to Android Beam file transfer.
+     *
+     * This content URI is the one you receive in the notification Intent.
+     *
+     * To get the directory of the first file, you retrieve it from MediaStore using the content URI.
+     */
+    private String handleContentUri(Uri beamUri) {
+        // Position of the filename in the query Cursor.
+        int filenameIndex;
+        // File object for the filename.
+        File copiedFile;
+        // The filename stored in MediaStore.
+        String fileName;
+        /* Test the authority of the URI - Determine the content provider.
+         *
+         * To determine if you can retrieve a file directory from the content URI,
+         * determine the the content provider associated with the URI by calling
+         * Uri.getAuthority() to get the URI's authority.
+         *
+         * The result has two possible values:
+         * - MediaStore.AUTHORITY
+         * - Any other authority value
+         */
+        if (!TextUtils.equals(beamUri.getAuthority(), MediaStore.AUTHORITY)) {
+            /*
+             * Handle content URIs for other content providers
+             */
+        }
+        // For a MediaStore content URI.
+        else {
+            // Get the column that contains the file name.
+            String[] projection = {MediaStore.MediaColumns.DATA};
+            Cursor pathCursor =
+                    getContentResolver().query(beamUri, projection,
+                            null, null, null);
+            // Check for a valid cursor.
+            if (pathCursor != null &&
+                    pathCursor.moveToFirst()) {
+                // Get the column index in the Cursor.
+                filenameIndex = pathCursor.getColumnIndex(
+                        MediaStore.MediaColumns.DATA);
+                // Get the full file name including path.
+                fileName = pathCursor.getString(filenameIndex);
+                // Create a File object for the filename.
+                copiedFile = new File(fileName);
+                // Return the parent directory of the file.
+                return copiedFile.getParent();
+            } else {
+                // The query didn't work; return null.
+                return null;
+            }
+        }
+        return null;
+    }
 }
