919e2c72c072f42820e10bbcd31af81307d6609a
George Spiridakis <spiridakis.grg@gmail.com>
2016-06-28 00:00:14 +0300

    Get the Directory for Copied Files.

--- 02. Building Apps with Content Sharing/03. Sharing Files with NFC/ReceiveFilesWithNFC/app/src/main/java/com/codeburrow/receivefileswithnfc/MainActivity.java
+++ 02. Building Apps with Content Sharing/03. Sharing Files with NFC/ReceiveFilesWithNFC/app/src/main/java/com/codeburrow/receivefileswithnfc/MainActivity.java
@@ -1,13 +1,55 @@
 package com.codeburrow.receivefileswithnfc;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    // A File object containing the path to the transferred files
+    private File mParentPath;
+    // Incoming Intent
+    private Intent mIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called from onNewIntent() for a SINGLE_TOP Activity
+     * or onCreate() for a new Activity.
+     *
+     * For onNewIntent(), remember to call setIntent() to store the most
+     * current Intent.
+     */
+    private void handleViewIntent() {
+        // Get the Intent action
+        mIntent = getIntent();
+        String action = mIntent.getAction();
+        /*
+         * For ACTION_VIEW, the Activity is being asked to display data.
+         * Get the URI.
+         */
+        if (TextUtils.equals(action, Intent.ACTION_VIEW)) {
+            // Get the URI from the Intent
+            Uri beamUri = mIntent.getData();
+            /*
+             * Test for the type of URI, by getting its scheme value
+             */
+            if (TextUtils.equals(beamUri.getScheme(), "file")) {
+                mParentPath = handleFileUri(beamUri);
+            } else if (TextUtils.equals(
+                    beamUri.getScheme(), "content")) {
+                mParentPath = handleContentUri(beamUri);
+            }
+        }
     }
 }
