0b674068d3df5e8136cb404ab413c022d2688f12
George Spiridakis <spiridakis.grg@gmail.com>
2016-06-30 20:14:02 +0300

    Get the Thumbnail.

--- 03. Building Apps with Multimedia/02. Capturing Photos/CapturePhotos/app/src/main/java/com/codeburrow/capturephotos/MainActivity.java
+++ 03. Building Apps with Multimedia/02. Capturing Photos/CapturePhotos/app/src/main/java/com/codeburrow/capturephotos/MainActivity.java
@@ -1,21 +1,43 @@
 package com.codeburrow.capturephotos;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     // The request code for the take picture intent.
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    // Image View to display the picture captured by the user.
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageView = (ImageView) findViewById(R.id.result_picture_image_view);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            /*
+             * The Android Camera application encodes the photo in the return Intent
+             * delivered to onActivityResult() as a small Bitmap in the extras,
+             * under the key "data".
+             *
+             * The following code retrieves this image and displays it in an ImageView.
+             */
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
     }
 
     /**
