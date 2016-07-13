106675965bb36354342805b4e9b7828bc80f22cc
George Spiridakis <spiridakis.grg@gmail.com>
2016-07-13 14:04:11 +0300

    Load an HTML Document.

--- 03. Building Apps with Multimedia/03. Printing Content/PrintContent/app/src/main/java/com/codeburrow/printcontent/MainActivity.java
+++ 03. Building Apps with Multimedia/03. Printing Content/PrintContent/app/src/main/java/com/codeburrow/printcontent/MainActivity.java
@@ -9,6 +9,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewConfiguration;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import java.lang.reflect.Field;
@@ -16,6 +18,8 @@ import java.lang.reflect.Field;
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    // Load the HTML resource into the WebView object.
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +41,18 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Printing Photos", Toast.LENGTH_SHORT).show();
                 doPhotoPrint();
                 return true;
+            case R.id.menu_print_html_documents:
+                Toast.makeText(MainActivity.this, "Printing HTML Documents", Toast.LENGTH_SHORT).show();
+                doWebViewPrint();
+                return true;
             default:
                 return false;
         }
     }
 
     /**
+     * Helper Method.
+     * <p/>
      * Show the action overflow menu in devices with physical menu button (e.g. Samsung Note)
      */
     private void makeActionOverflowMenuShown() {
@@ -83,5 +93,71 @@ public class MainActivity extends AppCompatActivity {
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.droids);
         // Print the bitmap.
         photoPrinter.printBitmap("droids.jpg - test print", bitmap);
+    }
+
+    /**
+     * Helper Method.
+     * <p/>
+     * Creates a simple WebViewClient and load an HTML document created on the fly.
+     * <p/>
+     * Printing an HTML document with WebView involves
+     * - loading an HTML resource OR
+     * - building an HTML document as a string.
+     * <p/>
+     * This section describes how to build an HTML string and load it into a WebView for printing.
+     * This view object is typically used as part of an activity layout.
+     * However, if your application is not using a WebView,
+     * you can create an instance of the class specifically for printing purposes.
+     * <p/>
+     * The main steps for creating this custom print view are:
+     * 1. Create a WebViewClient that starts a print job after the HTML resource is loaded.
+     * 2. Load the HTML resource into the WebView object.
+     */
+    private void doWebViewPrint() {
+        // Create a WebView object specifically for printing.
+        WebView webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+                return false;
+            }
+
+            @Override
+            public void onPageFinished(WebView webView, String url) {
+                Log.e(LOG_TAG, "Page finished loading: " + url);
+
+                /*
+                 *  NOTE:
+                 *          Make sure your call for generating a print job happens
+                 *          in the onPageFinished() method of the WebViewClient.
+                 *          If you don't wait until page loading is finished,
+                 *          the print output may be incomplete or blank,
+                 *          or may fail completely.
+                 */
+//                createWebPrintJob(webView);
+                mWebView = null;
+            }
+        });
+        // Generate an HTML document on the fly:
+        String htmlDocument = "<html><body><h1>Test Content</h1><p>Testing, " +
+                "testing, testing...</p></body></html>";
+//        webView.loadDataWithBaseURL(null, htmlDocument, "text/HTML", "UTF-8", null);
+        /*
+         * If you want to include graphics in the page,
+         * place the graphic files in the assets/ directory of your project
+         * and specify a base URL in the first parameter of the loadDataWithBaseURL().
+          */
+//        webView.loadDataWithBaseURL("file:///android_asset/images/", htmlDocument, "text/HTML", "UTF-8", null);
+        /*
+         * You can also load a web page for printing
+         * by replacing the loadDataWithBaseURL() method with loadUrl()
+         *
+         * Print an existing web page (remember to request INTERNET permission!)
+         */
+        webView.loadUrl("http://developer.android.com/about/index.html");
+        // Hold an instance of the WebView object so that is it not garbage collected before the print job is created.
+        // Keep a reference to WebView object until you pass the PrintDocumentAdapter to the PrintManager.
+        mWebView = webView;
     }
 }
--- 03. Building Apps with Multimedia/03. Printing Content/README.md
+++ 03. Building Apps with Multimedia/03. Printing Content/README.md
@@ -30,3 +30,13 @@ Printing out content beyond a simple photo on Android requires composing text an
 The Android framework provides a way to use HTML to compose a document and print it with a minimum of code.
 In Android 4.4 (API level 19), the [WebView](https://developer.android.com/reference/android/webkit/WebView.html) class has been updated to enable printing HTML content.
 The class allows you to load a local HTML resource or download a page from the web, create a print job and hand it off to Android's print services.
+
+|When using [WebView](https://developer.android.com/reference/android/webkit/WebView.html) for creating print documents, you should be aware of the following limitations:|
+---
+|- You cannot add headers or footers, including page numbers, to the document.
+- The printing options for the HTML document do not include the ability to print page ranges, for example: Printing page 2 to 4 of a 10 page HTML document is not supported.|
+- An instance of WebView can only process one print job at a time.|
+- An HTML document containing CSS print attributes, such as landscape properties, is not supported.|
+- You cannot use JavaScript in a HTML document to trigger printing.|
+
+> **Note:** The content of a WebView object that is included in a layout can also be printed once it has loaded a document.
