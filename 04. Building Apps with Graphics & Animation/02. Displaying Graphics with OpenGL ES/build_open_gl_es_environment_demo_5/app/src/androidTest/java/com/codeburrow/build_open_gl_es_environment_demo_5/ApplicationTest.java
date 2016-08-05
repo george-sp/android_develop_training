ef0fb828ffeef287d1e7a0dad06c75d80546fa8b
George Spiridakis <spiridakis.grg@gmail.com>
2016-08-05 22:30:24 +0300

    Implement build_open_gl_es_environment_demo_5. Resolve #6.

---
+++ 04. Building Apps with Graphics & Animation/02. Displaying Graphics with OpenGL ES/build_open_gl_es_environment_demo_5/app/src/androidTest/java/com/codeburrow/build_open_gl_es_environment_demo_5/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.codeburrow.build_open_gl_es_environment_demo_5;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ 04. Building Apps with Graphics & Animation/02. Displaying Graphics with OpenGL ES/build_open_gl_es_environment_demo_5/app/src/test/java/com/codeburrow/build_open_gl_es_environment_demo_5/ExampleUnitTest.java
@@ -0,0 +1,16 @@
+package com.codeburrow.build_open_gl_es_environment_demo_5;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
