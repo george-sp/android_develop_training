cc07debaddef910bd088bcd0e8082dc2898844db
George Spiridakis <spiridakis.grg@gmail.com>
2016-08-05 21:26:52 +0300

    Apply Rotation.

--- 04. Building Apps with Graphics & Animation/02. Displaying Graphics with OpenGL ES/DisplayGraphicsWithOpenGLES/app/src/main/java/com/codeburrow/displaygraphicswithopengles/MyGLRenderer.java
+++ 04. Building Apps with Graphics & Animation/02. Displaying Graphics with OpenGL ES/DisplayGraphicsWithOpenGLES/app/src/main/java/com/codeburrow/displaygraphicswithopengles/MyGLRenderer.java
@@ -3,7 +3,6 @@ package com.codeburrow.displaygraphicswithopengles;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
-import android.os.SystemClock;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -83,12 +82,12 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         // Calculate the projection and view transformation.
         Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
 
-        // Create a rotation transformation for the triangle
-        long time = SystemClock.uptimeMillis() % 4000L;
-        float angle = 0.090f * ((int) time);
-        Matrix.setRotateM(mRotationMatrix, 0, angle, 0, 0, -1.0f);
+        // Create a rotation transformation for the triangle.
+//        long time = SystemClock.uptimeMillis() % 4000L;
+//        float angle = 0.090f * ((int) time);
+        Matrix.setRotateM(mRotationMatrix, 0, mAngle, 0, 0, -1.0f);
 
-        // Combine the rotation matrix with the projection and camera view
+        // Combine the rotation matrix with the projection and camera view.
         // Note that the mMVPMatrix factor *must be first* in order
         // for the matrix multiplication product to be correct.
         Matrix.multiplyMM(scratch, 0, mMVPMatrix, 0, mRotationMatrix, 0);
