bafa27a5aeba9c32d2091bee14ffce7148ea164f
George Spyridakis <spiridakis.grg@gmail.com>
2017-09-03 22:56:07 +0300

    Make NsdChatActivity full functional

--- 05. Building Apps with Connectivity & the Cloud/01. Connecting Devices Wirelessly/ConnectDevicesWirelessly/app/src/main/java/com/codeburrow/connectdeviceswirelessly/NsdChatActivity.java
+++ 05. Building Apps with Connectivity & the Cloud/01. Connecting Devices Wirelessly/ConnectDevicesWirelessly/app/src/main/java/com/codeburrow/connectdeviceswirelessly/NsdChatActivity.java
@@ -1,7 +1,13 @@
 package com.codeburrow.connectdeviceswirelessly;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class NsdChatActivity extends AppCompatActivity {
 
@@ -10,39 +16,103 @@ public class NsdChatActivity extends AppCompatActivity {
     NsdHelper mNsdHelper;
     ChatConnection mConnection;
 
+    private TextView mStatusView;
+    private Handler mUpdateHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(LOG_TAG, "Creating chat activity");
         setContentView(R.layout.activity_nsd_chat);
+
+        mStatusView = (TextView) findViewById(R.id.status);
+
+        mUpdateHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                String chatLine = msg.getData().getString("msg");
+                addChatLine(chatLine);
+            }
+        };
+    }
+
+    public void clickAdvertise(View view) {
+        // Register service
+        if (mConnection.getLocalPort() > -1) {
+            mNsdHelper.registerService(mConnection.getLocalPort());
+        } else {
+            Log.w(LOG_TAG, "ServerSocket isn't bound.");
+        }
+    }
+
+    public void clickDiscover(View v) {
+        mNsdHelper.discoverServices();
+    }
+
+    public void clickSend(View view) {
+        EditText messageView = (EditText) this.findViewById(R.id.chatInput);
+        if (messageView != null) {
+            String messageString = messageView.getText().toString();
+            if (!messageString.isEmpty()) {
+                mConnection.sendMessage(messageString);
+            }
+            messageView.setText("");
+        }
+    }
+
+    public void addChatLine(String line) {
+        mStatusView.append("\n" + line);
+    }
+
+    @Override
+    public void onStart() {
+        Log.d(LOG_TAG, "Starting");
+
+        mConnection = new ChatConnection(mUpdateHandler);
+        mNsdHelper = new NsdHelper(this);
+        mNsdHelper.initializeNsd();
+
+        super.onStart();
     }
 
     @Override
     protected void onPause() {
+        Log.d(LOG_TAG, "Pausing");
+
         if (mNsdHelper != null) {
-            mNsdHelper.tearDown();
+            mNsdHelper.stopDiscovery();
         }
+
         super.onPause();
     }
 
     @Override
     protected void onResume() {
+        Log.d(LOG_TAG, "Resuming");
+
         super.onResume();
+
         if (mNsdHelper != null) {
-            mNsdHelper.registerService(mConnection.getLocalPort());
             mNsdHelper.discoverServices();
         }
     }
 
     @Override
-    protected void onDestroy() {
+    protected void onStop() {
+        Log.d(LOG_TAG, "Being stopped");
+
         mNsdHelper.tearDown();
         mConnection.tearDown();
-        super.onDestroy();
+        mNsdHelper = null;
+        mConnection = null;
+
+        super.onStop();
     }
 
-    // NsdHelper's tearDown method
-    public void tearDown() {
-        mNsdManager.unregisterService(mRegistrationListener);
-        mNsdManager.stopServiceDiscovery(mDiscoveryListener);
+    @Override
+    protected void onDestroy() {
+        Log.d(LOG_TAG, "Being destroyed");
+
+        super.onDestroy();
     }
 }
